3bab6500169973afe55d5b9ef949e9139c993426
Iroda Xolbekova <irodaxolbekova2@gmail.com>
2023-10-31 20:38:46 +0500

    updated

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,18 +5,8 @@
  */
 
 #include <iostream>
-#include "task_1.h"
-
+using namespace std;
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    cout<<"nima";
     return 0;
 }
